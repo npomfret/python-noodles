42fc14c5dc19705e84df929540d19a427d45648a
nick pomfret <nick-github@snowmonkey.co.uk>
2023-01-20 10:48:32 +0000

    removed cols that don't appear to affect the result

--- dataquest - predict stock prices with Scikit-Learn/main.py
+++ dataquest - predict stock prices with Scikit-Learn/main.py
@@ -6,18 +6,21 @@ from sklearn.metrics import precision_score
 import pandas as pd
 import numpy as np
 
-msft = yf.Ticker("MSFT")
+symbol = "MSFT"
+msft = yf.Ticker(symbol)
 msft_hist = msft.history(period="max")
 
 data = msft_hist[['Close']]
 data = data.rename(columns={'Close': 'Actual_Close'})
-# 'price_up' will be a 1 if the close price was greater than the previous close price, 0 otherwise
-data['price_up'] = msft_hist.rolling(2).apply(lambda x: x.iloc[1] > x.iloc[0])['Close']
+# 'price_up_flag' will be a 1 if the close price was greater than the previous close price, 0 otherwise
+data['price_up_flag'] = msft_hist.rolling(2).apply(lambda x: x.iloc[1] > x.iloc[0])['Close']
 data['daily_return'] = msft_hist.rolling(2).apply(lambda x: x.iloc[1] / x.iloc[0])['Close']
 data.dropna(inplace=True)
 print(data.head(5))
-data['ln_ret'] = np.log(data["daily_return"])
+data['ln_daily_return'] = np.log(data["daily_return"])
 print(data.head(5))
+
+# Target is any row that has seen a 'large' one-day price increase
 data['Target'] = (data['daily_return'] > 1.005).astype(int)
 print(data.head(5))
 
@@ -28,67 +31,22 @@ predictors = ['Close', "High", "Low", "Open", "Volume"]
 
 data = data.join(msft_prev[predictors]).iloc[1:]
 # we dropped the first row above because there is no prior day
-print(data.head(5))
-
-# model = RandomForestClassifier(n_estimators=100, min_samples_split=200, random_state=1)
-#
-# train = data.iloc[:-100]
-# test = data.iloc[-100:]
-#
-# print("learning...")
-# model.fit(train[predictors], train["Target"])
-#
-# preds = model.predict_proba(test[predictors])
-# print(f'preds: {preds}')
-# preds = pd.Series(preds, index=test.index)
-#
-# score = precision_score(test["Target"], preds)
-# print(f'score: {score}')
-
-# combined = pd.concat({"price_up": test["price_up"], "Predictions": preds}, axis=1)
+print(data.tail(5))
 
 # create some synthetic columns
-short_mean = data.rolling(10).mean()
-medium_mean = data.rolling(30).mean()
-long_mean = data.rolling(90).mean()
-weekly_trend = data.shift(1).rolling(5).mean()["price_up"]
-weekly_ret = data.shift(1).rolling(5).sum()["ln_ret"]
-
-data["short_mean"] = short_mean["Close"] / data["Close"]
-data["medium_mean"] = medium_mean["Close"] / data["Close"]
-data["long_mean"] = long_mean["Close"] / data["Close"]
-
-data["long_mean_short_mean_ratio"] = data["long_mean"] / data["short_mean"]
-data["long_mean_medium_mean_ratio"] = data["long_mean"] / data["medium_mean"]
-data["weekly_trend"] = weekly_trend
-data["weekly_ret"] = weekly_ret
-
-data["open_close_ratio"] = data["Open"] / data["Close"]
-data["high_close_ratio"] = data["High"] / data["Close"]
-data["low_close_ratio"] = data["Low"] / data["Close"]
+data["weekly_trend"] = data.shift(1).rolling(5).mean()["price_up_flag"]
+data["weekly_ret"] = data.shift(1).rolling(5).sum()["ln_daily_return"]
 
 data.dropna(inplace=True)
 
-full_predictors = predictors + ["short_mean", "medium_mean", "long_mean",
-                                "long_mean_short_mean_ratio", "long_mean_medium_mean_ratio",
-                                "open_close_ratio", "high_close_ratio", "low_close_ratio",
-                                "weekly_trend",
-                                "price_up", "daily_return", "weekly_ret"
-                                ]
-# train = data.iloc[:-100]
-# test = data.iloc[-100:]
-#
-model = RandomForestClassifier(n_estimators=25, min_samples_split=3, random_state=1)
-
-# print("learning...")
-# model.fit(train[full_predictors], train["Target"])
-#
-# preds = model.predict_proba(test[full_predictors])
-# print(f'preds: {preds}')
-# print(f'')
+full_predictors = predictors + [
+    "weekly_trend",
+    "price_up_flag",
+    "daily_return",
+    "weekly_ret"
+]
 
 def backtest(data, model, predictor_cols, train_size, test_size=1):
-
     all_preds = []
     threshold = .6
     row_count = data.shape[0]
@@ -144,6 +102,8 @@ def backtest(data, model, predictor_cols, train_size, test_size=1):
 
     return pd.concat(all_preds)
 
+
+model = RandomForestClassifier(n_estimators=25, min_samples_split=3, random_state=1)
 
 final_predictions = backtest(data.iloc[-(500 * 1):], model, full_predictors, 25, 1)
 print('value counts for predictions:')
