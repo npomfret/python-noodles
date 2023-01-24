5c0d3788926be34b2e382fed677d14c346e855a6
nick pomfret <nick-github@snowmonkey.co.uk>
2023-01-24 16:16:09 +0000

    moved model evaluation stuff into it's own class

--- alphavantage LSTM/main.py
+++ alphavantage LSTM/main.py
@@ -1,10 +1,7 @@
-import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
 from torch.utils.data import DataLoader
 from market_data import download_price_history, create_windowed_data, create_output_array
-from model import LSTMModel, TimeSeriesDataset, Normalizer
+from model import LSTMModel, TimeSeriesDataset, Normalizer, LSTMModelDefinition
 from plots import plot_predict_unseen, plot_train_vs_test, plot_predictions_vs_actual, plot_predictions_vs_actual_zoomed
 
 config = {
@@ -56,88 +53,28 @@ print(f'Testing data shape, x: {dataset_test.x.shape}, y: {dataset_test.y.shape}
 batch_size = config["training"]["batch_size"]
 hw_device = config["training"]["device"]
 
-
-def run_epoch(dataloader, is_training=False):
-    epoch_loss = 0
-
-    if is_training:
-        model.train()
-    else:
-        model.eval()
-
-    for idx, (x_tensor, y_tensor) in enumerate(dataloader):
-        if is_training:
-            optimizer.zero_grad()
-
-        x_tensor = x_tensor.to(hw_device)
-        y_tensor = y_tensor.to(hw_device)
-
-        out_tensor = model(x_tensor)
-        loss = criterion(out_tensor.contiguous(), y_tensor.contiguous())
-
-        if is_training:
-            loss.backward()
-            optimizer.step()
-
-        bs = x_tensor.shape[0]
-        epoch_loss += (loss.detach().item() / bs)
-
-    learning_rate = scheduler.get_last_lr()[0]
-
-    return epoch_loss, learning_rate
-
-
 training_dataloader = DataLoader(dataset_train, batch_size=batch_size, shuffle=True)
 testing_dataloader = DataLoader(dataset_test, batch_size=batch_size, shuffle=True)
 
-model = LSTMModel(output_size=1)
-model = model.to(hw_device)
-
-criterion = nn.MSELoss()
-optimizer = optim.Adam(
-    model.parameters(),
-    lr=config["training"]["learning_rate"],
-    betas=(0.9, 0.98),
-    eps=1e-9
-)
-scheduler = optim.lr_scheduler.StepLR(
-    optimizer,
-    step_size=config["training"]["scheduler_step_size"],
-    gamma=0.1
+model = LSTMModel(
+    LSTMModelDefinition(output_size=1),
+    hw_device,
+    config["training"]["learning_rate"],
+    config["training"]["scheduler_step_size"]
 )
 
-number_of_epochs = config["training"]["num_epoch"]
-
-for epoch in range(number_of_epochs):
-    loss_train, lr_train = run_epoch(training_dataloader, is_training=True)
-    loss_test, lr_test = run_epoch(testing_dataloader)
-    scheduler.step()
-
-    print(f'Epoch[{epoch + 1}/{number_of_epochs}], training loss:{loss_train:.6f}, testing loss:{loss_test:.6f}, lr:{lr_train:.6f}]')
+model.learn(
+    config["training"]["num_epoch"],
+    training_dataloader,
+    testing_dataloader
+)
 
 # here we re-initialize dataloader so the data isn't shuffled, so we can plot the values by date
-
 training_dataloader = DataLoader(dataset_train, batch_size=batch_size, shuffle=False)
 testing_dataloader = DataLoader(dataset_test, batch_size=batch_size, shuffle=False)
 
-# Set the module in evaluation mode
-model.eval()
-
-
-def make_predictions(data_loader):
-    res = np.array([])
-
-    for idx, (x, y) in enumerate(data_loader):
-        x = x.to(hw_device)
-        out = model(x)
-        out = out.cpu().detach().numpy()
-        res = np.concatenate((res, out))
-
-    return res
-
-
-predicted_train = make_predictions(training_dataloader)
-predicted_test = make_predictions(testing_dataloader)
+predicted_train = model.make_predictions(training_dataloader)
+predicted_test = model.make_predictions(testing_dataloader)
 
 plot_predictions_vs_actual(window_size, split_index, scaler, predicted_train, predicted_test, dates, close_prices)
 
@@ -145,10 +82,8 @@ plot_predictions_vs_actual_zoomed(scaler, data_y_test, predicted_test, dates, sp
 
 # predict the closing price of the next trading day
 
-model.eval()
-
 x = torch.tensor(data_x_unseen).float().to(hw_device).unsqueeze(0).unsqueeze(2)  # this is the data type and shape required, [batch, sequence, feature]
-prediction = model(x)
+prediction = model.make_prediction(x)
 prediction = prediction.cpu().detach().numpy()
 
 plot_predict_unseen(scaler, data_y_test, predicted_test, prediction, dates)
--- alphavantage LSTM/model.py
+++ alphavantage LSTM/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from torch import nn as nn
+import torch.optim as optim
 from torch.utils.data import Dataset
 
 
@@ -49,7 +50,7 @@ LSTM_CONFIG = {
 }
 
 
-class LSTMModel(nn.Module):
+class LSTMModelDefinition(nn.Module):
     def __init__(self, output_size=1):
         super().__init__()
 
@@ -99,3 +100,80 @@ class LSTMModel(nn.Module):
         tensor = self.dropout(tensor)
         predictions = self.linear_2(tensor)
         return predictions[:, -1]
+
+
+class LSTMModel:
+    def __init__(self, model_definition, hw_device, learning_rate, scheduler_step_size):
+        super().__init__()
+
+        self.model_def = model_definition.to(hw_device)
+        self.hw_device = hw_device
+
+        self.criterion = nn.MSELoss()
+        self.optimizer = optim.Adam(
+            model_definition.parameters(),
+            lr=learning_rate,
+            betas=(0.9, 0.98),
+            eps=1e-9
+        )
+        self.scheduler = optim.lr_scheduler.StepLR(
+            self.optimizer,
+            step_size=scheduler_step_size,
+            gamma=0.1
+        )
+
+    def learn(self, number_of_epochs, training_dataloader, testing_dataloader):
+        for epoch in range(number_of_epochs):
+            loss_train, learning_rate_train = self.run_epoch(training_dataloader, is_training=True)
+            loss_test, learning_rate_test = self.run_epoch(testing_dataloader)
+            self.scheduler.step()
+
+            print(f'Epoch[{epoch + 1}/{number_of_epochs}], training loss:{loss_train:.6f}, testing loss:{loss_test:.6f}, lr:{learning_rate_train:.6f}]')
+
+        # Set the module in evaluation mode from here
+        self.model_def.eval()
+
+    def run_epoch(self, dataloader, is_training=False):
+        epoch_loss = 0
+
+        if is_training:
+            self.model_def.train()
+        else:
+            self.model_def.eval()
+
+        for idx, (x_tensor, y_tensor) in enumerate(dataloader):
+            if is_training:
+                self.optimizer.zero_grad()
+
+            x_tensor = x_tensor.to(self.hw_device)
+            y_tensor = y_tensor.to(self.hw_device)
+
+            out_tensor = self.model_def(x_tensor)
+            loss = self.criterion(out_tensor.contiguous(), y_tensor.contiguous())
+
+            if is_training:
+                loss.backward()
+                self.optimizer.step()
+
+            batch_size = x_tensor.shape[0]
+            epoch_loss += (loss.detach().item() / batch_size)
+
+        learning_rate = self.scheduler.get_last_lr()[0]
+
+        return epoch_loss, learning_rate
+
+    def make_prediction(self, x_tensor):
+        self.model_def.eval()
+        return self.model_def(x_tensor)
+
+    def make_predictions(self, data_loader):
+        self.model_def.eval()
+        res = np.array([])
+
+        for idx, (x, y) in enumerate(data_loader):
+            x = x.to(self.hw_device)
+            out = self.model_def(x)
+            out = out.cpu().detach().numpy()
+            res = np.concatenate((res, out))
+
+        return res
