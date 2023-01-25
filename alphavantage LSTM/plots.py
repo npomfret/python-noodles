8e369f31698c87dafb1a386144accee3d2771bf3
nick pomfret <nick-github@snowmonkey.co.uk>
2023-01-25 11:50:49 +0000

    tidy

--- alphavantage LSTM/plots.py
+++ alphavantage LSTM/plots.py
@@ -9,8 +9,8 @@ CONFIG = {
     "color_train": "#3D9970",
     "color_val": "#0074D9",
     "color_pred_train": "#3D9970",
-    "color_pred_val": "#0074D9",
-    "color_pred_test": "#FF4136",
+    "color_pred_test": "#0074D9",
+    "color_pred_unseen": "#FF4136",
 }
 
 
@@ -85,7 +85,7 @@ def plot_predictions_vs_actual(lstm_data, predicted_train, predicted_test, price
     fig.patch.set_facecolor((1.0, 1.0, 1.0))
     plt.plot(price_history.dates, price_history.prices, label="Actual prices", color=CONFIG["color_actual"])
     plt.plot(price_history.dates, to_plot_data_y_train_pred, label="Predicted prices (train)", color=CONFIG["color_pred_train"])
-    plt.plot(price_history.dates, to_plot_data_y_val_pred, label="Predicted prices (validation)", color=CONFIG["color_pred_val"])
+    plt.plot(price_history.dates, to_plot_data_y_val_pred, label="Predicted prices (validation)", color=CONFIG["color_pred_test"])
 
     title = "Compare predicted prices to actual prices"
 
@@ -112,7 +112,7 @@ def plot_predictions_vs_actual_zoomed(lstm_data, predicted_val, dates):
     fig = figure(figsize=(25, 5), dpi=80)
     fig.patch.set_facecolor((1.0, 1.0, 1.0))
     plt.plot(to_plot_data_date, to_plot_data_y_val_subset, label="Actual prices", color=CONFIG["color_actual"])
-    plt.plot(to_plot_data_date, to_plot_predicted_val, label="Predicted prices (validation)", color=CONFIG["color_pred_val"])
+    plt.plot(to_plot_data_date, to_plot_predicted_val, label="Predicted prices (validation)", color=CONFIG["color_pred_test"])
     xticks = [to_plot_data_date[i] if ((i % int(ticks_interval / 5) == 0 and (len(to_plot_data_date) - i) > ticks_interval / 6) or i == len(to_plot_data_date) - 1) else None for i in range(len(to_plot_data_date))]  # make x ticks nice
 
     title = "Zoom in to examine predicted price on validation data portion"
@@ -132,27 +132,29 @@ def plot_predict_unseen(lstm_data, predicted_val, unseen_prediction, price_histo
 
     # prepare plots
     plot_range = 10
-    to_plot_data_y_val = np.zeros(plot_range)
-    to_plot_data_y_val_pred = np.zeros(plot_range)
+    to_plot_data_y_test = np.zeros(plot_range)
+    to_plot_data_y_test[:plot_range - 1] = lstm_data.unscale(lstm_data.data_y_test)[-plot_range + 1:]
+    to_plot_data_y_test = np.where(to_plot_data_y_test == 0, None, to_plot_data_y_test)
+    
     to_plot_data_y_test_pred = np.zeros(plot_range)
-    to_plot_data_y_val[:plot_range - 1] = lstm_data.unscale(lstm_data.data_y_test)[-plot_range + 1:]
-    to_plot_data_y_val_pred[:plot_range - 1] = lstm_data.unscale(predicted_val)[-plot_range + 1:]
-    to_plot_data_y_test_pred[plot_range - 1] = lstm_data.unscale(unseen_prediction)
-    to_plot_data_y_val = np.where(to_plot_data_y_val == 0, None, to_plot_data_y_val)
-    to_plot_data_y_val_pred = np.where(to_plot_data_y_val_pred == 0, None, to_plot_data_y_val_pred)
+    to_plot_data_y_test_pred[:plot_range - 1] = lstm_data.unscale(predicted_val)[-plot_range + 1:]
     to_plot_data_y_test_pred = np.where(to_plot_data_y_test_pred == 0, None, to_plot_data_y_test_pred)
+    
+    to_plot_data_y_unseen_pred = np.zeros(plot_range)
+    to_plot_data_y_unseen_pred[plot_range - 1] = lstm_data.unscale(unseen_prediction)
+    to_plot_data_y_unseen_pred = np.where(to_plot_data_y_unseen_pred == 0, None, to_plot_data_y_unseen_pred)
 
     # plot
     plot_date_test = price_history.dates[-plot_range + 1:]
     plot_date_test.append("tomorrow")
     fig = figure(figsize=(25, 5), dpi=80)
     fig.patch.set_facecolor((1.0, 1.0, 1.0))
-    plt.plot(plot_date_test, to_plot_data_y_val, label="Actual prices", marker=".", markersize=10, color=CONFIG["color_actual"])
-    plt.plot(plot_date_test, to_plot_data_y_val_pred, label="Past predicted prices", marker=".", markersize=10, color=CONFIG["color_pred_val"])
-    plt.plot(plot_date_test, to_plot_data_y_test_pred, label="Predicted price for next day", marker=".", markersize=20, color=CONFIG["color_pred_test"])
+    plt.plot(plot_date_test, to_plot_data_y_test, label="Actual prices", marker=".", markersize=10, color=CONFIG["color_actual"])
+    plt.plot(plot_date_test, to_plot_data_y_test_pred, label="Past predicted prices", marker=".", markersize=10, color=CONFIG["color_pred_test"])
+    plt.plot(plot_date_test, to_plot_data_y_unseen_pred, label="Predicted price for next day", marker=".", markersize=20, color=CONFIG["color_pred_unseen"])
     plt.title(f'Predicting the close price of the next trading day ({next_day_string})')
     plt.grid(b=None, which='major', axis='y', linestyle='--')
     plt.legend()
     plt.show()
 
-    print(f'Predicted close price of the next trading day ({next_day_string}): {round(to_plot_data_y_test_pred[plot_range - 1], 2)}')
+    print(f'Predicted close price of the next trading day ({next_day_string}): {round(to_plot_data_y_unseen_pred[plot_range - 1], 2)}')
