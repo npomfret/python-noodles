a8226c6780c7341620efcde70ecefd8cd6caecdf
nick pomfret <nick-github@snowmonkey.co.uk>
2023-02-20 14:21:24 +0000

    gradient descent with optimizer

--- Patrick Loeber/main.py
+++ Patrick Loeber/main.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import torch.nn as nn
 
 X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
 Y = torch.tensor([4, 8, 12, 16], dtype=torch.float32)
@@ -11,29 +12,23 @@ def forward(x):
     return w * x
 
 
-def loss(y, y_predicted):
-    return ((y_predicted - y) ** 2).mean()
-
-
-
 print(f'prediction before training: f(5) = {forward(5):.3f} (it should be 20)')
 
 # training
 learning_rate = 0.01
 n_iters = 50
+loss = nn.MSELoss()
+optimizer = torch.optim.SGD([w], lr=learning_rate)
 
 for epoch in range(n_iters):
     y_pred = forward(X)
 
     l = loss(Y, y_pred)
     l.backward()
+    optimizer.step()
+    optimizer.zero_grad()
 
     if(epoch % 5 == 0):
         print (f'{epoch}: w = {w:.3f}, loss = {l:.8f}')
-
-    with torch.no_grad():
-        w -= learning_rate * w.grad
-
-    w.grad.zero_()
 
 print(f'prediction after training: f(5) = {forward(5):.3f}')
